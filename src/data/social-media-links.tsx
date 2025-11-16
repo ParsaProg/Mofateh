8b97db97cdd890dcc00da5da47fe6876760b46fb
ParsaProg <parsashaabani3@gmail.com>
2025-11-16 18:54:20 +0330

    oscial media containers data

---
+++ src/data/social-media-links.tsx
@@ -0,0 +1,25 @@
+import { Mail, Phone } from "lucide-react";
+
+export const socialMediaLinks = [
+    {
+        icon: <Mail size={20}/>,
+        link: "mailto:parsashaabani3@gmail.com",
+        text: "ایمیل مدرسه"
+    },
+    {
+        icon: <Mail size={20}/>,
+        link: "mailto:parsashaabani3@gmail.com",
+        text: "ایتای مدرسه"
+    },
+    {
+        icon: <Mail size={20}/>,
+        link: "mailto:parsashaabani3@gmail.com",
+        text: "سروش مدرسه"
+    },
+    {
+        icon: <Phone size={18}/>,
+        link: "call:+989915980854",
+        text: "تماس با مدرسه"
+    },
+    
+]
