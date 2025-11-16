759aef6cb125539418e052f97e67709ac9db9e93
ParsaProg <parsashaabani3@gmail.com>
2025-11-16 18:51:36 +0330

    page.tsx main changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,8 @@
+import HeroSection from "@/components/landing/HeroSection";
+
 export default function Home(){
-  return <div className="text-black">سلام</div>
+  return <div className="text-black">
+    <HeroSection />
+    <div className="h-[100px]"></div>
+  </div>
 }
