1981663b7873d497c27bbe8a6bae0c03681c4055
ParsaProg <parsashaabani3@gmail.com>
2025-11-17 22:45:19 +0330

    responsive + fix dimensions

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import HeroSection from "@/components/landing/HeroSection";
+import TeachersLanding from "@/components/landing/Teachers";
 
 export default function Home() {
   return (
     <div className="text-black">
       <HeroSection />
+      <TeachersLanding />
     </div>
   );
 }
