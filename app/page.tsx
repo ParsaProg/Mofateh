6529e4e937e01449a9806cfa97ce949c1d8247ed
ParsaProg <parsashaabani3@gmail.com>
2025-11-19 22:53:51 +0330

    add picture gallery list section in the main lanfing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,14 @@
 import HeroSection from "@/components/landing/HeroSection";
 import TeachersLanding from "@/components/landing/Teachers";
+import GallerySection from "@/components/landing/GallerySection";
 
 export default function Home() {
   return (
     <div className="text-black">
       <HeroSection />
+      <GallerySection />
       <TeachersLanding />
+      <div className="mt-100"></div>
     </div>
   );
 }
