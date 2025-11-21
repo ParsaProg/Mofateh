969ba067a04afe5072513956fd5a3af9d1dd9c86
ParsaProg <parsashaabani3@gmail.com>
2025-11-22 02:55:58 +0330

    page.tsx main changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,14 @@
 import HeroSection from "@/components/landing/HeroSection";
 import TeachersLanding from "@/components/landing/Teachers";
 import GallerySection from "@/components/landing/SchoolIntrducing";
+import { SchoolWorkers } from "@/components/landing/SchoolWorkers";
 
 export default function Home() {
   return (
     <div className="text-black">
       <HeroSection />
       <GallerySection />
-      <TeachersLanding />
+      <SchoolWorkers />
       <div className="mt-100"></div>
     </div>
   );
