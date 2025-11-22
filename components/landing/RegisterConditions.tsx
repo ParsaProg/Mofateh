81c29311bae8f092878095fee7017887bf81448f
ParsaProg <parsashaabani3@gmail.com>
2025-11-23 01:25:28 +0330

    responsive > register cnodition landing section

--- components/landing/RegisterConditions.tsx
+++ components/landing/RegisterConditions.tsx
@@ -4,7 +4,7 @@ export default function RegisterConditionsLanding() {
   return (
     <div className="w-full mt-16">
       <LandingSectionTitle title="شریط ثبت‌نام" />
-      <section className="flex items-center gap-x-10 mt-8">
+      <section className="flex max-[920px]:flex-col items-center gap-10 mt-8">
         <div className="relative w-full rounded-2xl h-[600px] shadow-blue-200 shadow-[0px_0px_20px_5px]">
             <div className="bg-blue-500 rounded-2xl w-full h-[300px]"></div>
             <div className="p-5 rounded-2xl bg-[#ffffff7d] backdrop-blur-xl shadow-[#ffffff72] shadow-[0px_0px_20px_3px] mx-auto w-[95%] h-[400px] -mt-[100px]"></div>
