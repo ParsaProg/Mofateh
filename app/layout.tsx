0910e8ee5b797037cd03a7180e9ad24e6f90823f
ParsaProg <parsashaabani3@gmail.com>
2025-11-23 01:11:08 +0330

    add scrolltoTopButton

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import Header from "@/components/header/Header";
 import SmoothScrollProvider from "@/src/providers/smooth-scroll";
+import TopScrollButton from "@/components/ui/top-scroll-button/TopScrollButton";
 
 export const metadata: Metadata = {
   title: "دبیرستان نمونه دولتی شهید مفتح",
@@ -17,6 +18,7 @@ export default function RootLayout({
     <html suppressHydrationWarning={true} lang="fa" dir="rtl">
       <body className="w-[90%] mx-auto">
         <Header />
+        <TopScrollButton />
         <div className="mt-20"></div>
         <SmoothScrollProvider>{children}</SmoothScrollProvider>
       </body>
