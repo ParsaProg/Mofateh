db9fbe3b96333963664fc95b31aeafaff99d5419
ParsaProg <parsashaabani3@gmail.com>
2025-11-23 01:13:32 +0330

    school-workers data changes

--- src/data/school-workers.ts
+++ src/data/school-workers.ts
@@ -1,53 +1,63 @@
 export const SchoolWorkersData = [
-    {
-        name: "",
-        subName: "",
-        picture: ""
-    },
-    {
-        name: "",
-        subName: "",
-        picture: ""
-    },
-    {
-        name: "",
-        subName: "",
-        picture: ""
-    },
-    {
-        name: "",
-        subName: "",
-        picture: ""
-    },
-    {
-        name: "",
-        subName: "",
-        picture: ""
-    },
-    {
-        name: "",
-        subName: "",
-        picture: ""
-    },
+  {
+    name: "",
+    subName: "",
+    picture: "",
+  },
+  {
+    name: "",
+    subName: "",
+    picture: "",
+  },
+  {
+    name: "",
+    subName: "",
+    picture: "",
+  },
+  {
+    name: "",
+    subName: "",
+    picture: "",
+  },
+  {
+    name: "",
+    subName: "",
+    picture: "",
+  },
+  {
+    name: "",
+    subName: "",
+    picture: "",
+  },
 
-    {
-        name: "",
-        subName: "",
-        picture: ""
-    },
-    {
-        name: "",
-        subName: "",
-        picture: ""
-    },
-    {
-        name: "",
-        subName: "",
-        picture: ""
-    },
-    {
-        name: "",
-        subName: "",
-        picture: ""
-    },
-]
+  {
+    name: "",
+    subName: "",
+    picture: "",
+  },
+  {
+    name: "",
+    subName: "",
+    picture: "",
+  },
+  {
+    name: "",
+    subName: "",
+    picture: "",
+  },
+  {
+    name: "",
+    subName: "",
+    picture: "",
+  },
+  {
+    name: "",
+    subName: "",
+    picture: "",
+  },
+  {
+    name: "",
+    subName: "",
+    picture: "",
+  },
+];
